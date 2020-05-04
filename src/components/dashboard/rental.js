7ad0799f0a48a183093e664c62adf82a5b83a49e
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-05-05 04:06:08 +0900

    書籍一覧の表示

--- src/components/dashboard/rental.js
+++ src/components/dashboard/rental.js
@@ -1,18 +1,45 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'; 
 import * as Actions from '../../actions';
+import _ from 'lodash';
+import Slider from 'react-slick';
+import * as Setting from '../../constants/slider';
+
+import '../../stylesheets/slick.css';
+import '../../stylesheets/slick-theme.css';
 
 class Rental extends Component {
   componentDidMount() {
     // 初期時にとってきて更新ごとに追加するか、毎回全部取ってくるか、検討中
     // とりあえず、全部とってくる
-    //this.props.requestAllBookdata(this.props.login.token)
-    this.props.requestAllBookdata("kenji")
+    this.props.requestAllBookdata(this.props.login.token)
+  }
+  showDBBook() {
+    if(this.props.book.length === 0) {
+      return null
+    }
+    return _.map(this.props.book.storeditems, (value, key, object) => (
+      <div key={key}>
+        <img
+          alt={value.title}
+          src={value.imageLink === undefined ? null : value.imageLink}
+        />
+      </div>
+    ))
+    
   }
   
   render() {
+    const showDBBook = this.showDBBook();
+                
     return (
-      <h2>Showing Rental page</h2>
+      <div className="slideBooks">
+        <h3>書籍一覧</h3>
+        <p>その他の表示</p>
+        <Slider {...Setting.slider}>
+          {showDBBook}
+        </Slider>
+      </div>
     )
   }
 }
